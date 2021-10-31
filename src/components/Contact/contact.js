7d37d156562a5d9a816efd3f0478b4913aad4433
assilkld <khlifiassil@gmail.com>
2021-10-31 12:34:40 +0100

    Update Contact Form
    Contact Form

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,20 +1,74 @@
-import './contact.css';
+import './fonts/icomoon/style.css';
+import './css/bootstrap.min.css';
+import './css/style.css';
+
 const Contact = () => {
     return (
-        <div class="container">       
-             <form class="form">
-            
-            <h2>CONTACTEZ NOUS</h2>
-            <p type="Nom:"><input placeholder="Votre Nom.."></input></p>
-            <p type="Email:"><input placeholder="Votre Email.."></input></p>
-            <p type="Message:"><input placeholder="Votre Message.."></input></p>
-            <button>Send Message</button>
-            <div>
-                <span class="fa fa-phone"></span>001 1023 567
-                <span class="fa fa-envelope-o"></span> contact@company.com
+        <div class="content">
+
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-10">
+
+
+                        <div class="row justify-content-center">
+                            <div class="col-md-6">
+
+                                <h3 class="heading mb-4">Let's talk about everything!</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas debitis, fugit natus?</p>
+
+                            </div>
+                            <div class="col-md-6">
+
+                                <form class="mb-5" method="post" id="contactForm" name="contactForm">
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            <input type="text" class="form-control" name="name" id="name" placeholder="Your name" />
+
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            <input type="text" class="form-control" name="email" id="email" placeholder="Email" />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            <textarea class="form-control" name="message" id="message" cols="30" rows="7" placeholder="Write your message"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <input type="submit" value="Send Message" class="btn btn-primary rounded-0 py-2 px-4" />
+                                            <span class="submitting"></span>
+                                        </div>
+                                    </div>
+                                </form>
+
+                                <div id="form-message-warning mt-4"></div>
+                                <div id="form-message-success">
+                                    Your message was sent, thank you!
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </form>
+            <script src="js/jquery-3.3.1.min.js"></script>
+            <script src="js/popper.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/jquery.validate.min.js"></script>
+            <script src="js/main.js"></script>
         </div>
+
+
+
+
 
     )
 }
