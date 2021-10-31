fc34c116b6f973798cd34cd3bef846bc82a84cae
assilkld <khlifiassil@gmail.com>
2021-10-31 10:56:26 +0000

    update From Contact

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,18 +1,21 @@
 import './contact.css';
 const Contact = () => {
     return (
-
-        <form class="form">
-            <h2>CONTACT US</h2>
-            <p type="Name:"><input placeholder="Write your name here.."></input></p>
-            <p type="Email:"><input placeholder="Let us know how to contact you back.."></input></p>
-            <p type="Message:"><input placeholder="What would you like to tell us.."></input></p>
+        <div class="container">       
+             <form class="form">
+            
+            <h2>CONTACTEZ NOUS</h2>
+            <p type="Nom:"><input placeholder="Votre Nom.."></input></p>
+            <p type="Email:"><input placeholder="Votre Email.."></input></p>
+            <p type="Message:"><input placeholder="Votre Message.."></input></p>
             <button>Send Message</button>
             <div>
                 <span class="fa fa-phone"></span>001 1023 567
                 <span class="fa fa-envelope-o"></span> contact@company.com
             </div>
         </form>
+        </div>
+
     )
 }
 
