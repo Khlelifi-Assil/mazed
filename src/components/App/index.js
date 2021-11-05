8bd4fb3c3364b856fd9d92e44db19b568ec61081
assilkld <khlifiassil@gmail.com>
2021-11-05 13:03:17 +0100

    Update Encherir

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,7 +6,7 @@ import Register from '../SignUp/index';
 import Annonce from '../Annonce/annonce';
 import Admin from '../Admin/src/App' ;
 import Contact from '../Contact/contact';
-import Home from '../Home/home';
+import Encherir from '../Encherir/encherir';
 function App() {
   
   return (
@@ -18,7 +18,7 @@ function App() {
       <Route exact path='/annonce' component={Annonce} />
       <Route exact path='/admin' component={Admin} />
       <Route exact path='/contact' component={Contact} />
-      <Route exact path='/home' component={Home} />
+      <Route exact path='/encherir' component={Encherir} />
      </div>
      </Router>
   );
---
+++ src/components/Encherir/encherir.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+//import Frame from 'react-frame-component';
+import Popup from "reactjs-popup";
+import firebase from "firebase";
+import "firebase/firestore";
+import db from "../../services/firestore";
+import "./encherir.css";
+
+const Encherir = () => {
+   
+
+  const [info, setInfo] = useState([]);
+
+  // Start the fetch operation as soon as
+  // the page loads
+  window.addEventListener("load", () => {
+    Fetchdata();
+  });
+
+  // Fetch the required data using the get() method
+  const Fetchdata = () => {
+    db.collection("annonces")
+      .get()
+      .then((querySnapshot) => {
+        // Loop through the data and store
+        // it in array to display
+        querySnapshot.forEach((element) => {
+          var data = element.data();
+          setInfo((arr) => [...arr, data]);
+        });
+      });
+  };
+
+  return (
+      
+    <div>
+      {info.map((data) => (
+        <Frame
+          titre={data.titre}
+          description={data.description}
+          prix={data.prix}
+          date={data.date}
+          livraison={data.livraison}
+          paiement={data.paiement}
+          telephone={data.telephone}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Frame = ({
+  titre,
+  description,
+  prix,
+  date,
+  livraison,
+  paiement,
+  telephone,
+}) => {
+  return (
+    <center>
+      <div id="container">
+        <div class="product-details">
+          <center>
+            <h1> {titre}</h1>
+          </center>
+          <br />
+          <div class="div-info">
+            Paiement : {paiement} <br />
+            livraison : {livraison} <br />
+            Tel : {telephone}
+          </div>
+          <span class="hint-star star">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half-o" aria-hidden="true"></i>
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+          </span>
+
+          <div class="control">
+            <button class="btn">
+              <span class="price">{prix} TND</span>
+              <span class="shopping-cart">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+              </span>
+              <span class="buy">Enchérir</span>
+            </button>
+          </div>
+        </div>
+        <div class="product-image">
+          <img
+            src="https://sc01.alicdn.com/kf/HTB1Cic9HFXXXXbZXpXXq6xXFXXX3/200006212/HTB1Cic9HFXXXXbZXpXXq6xXFXXX3.jpg"
+            alt="Omar Dsoky"
+          />
+          <div class="info">
+            <h2>The Description</h2>
+            <center>{description}</center>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
+};
+
+export default Encherir;
